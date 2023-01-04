c3ccc9b6e50a40b38e772481675860d8dafcb05f
Harrynoqs <noqsnoqs@gmail.com>
2023-01-04 16:25:21 +0300

    Modified ruby files

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ require './trimmer_decorator'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -28,5 +28,9 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals << rental
   end
 end
